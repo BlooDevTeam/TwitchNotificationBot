85995757807db46f4dc3e8f34fd654a0e7070aa9
BlooDevTeam <105457368+BlooDevTeam@users.noreply.github.com>
2022-05-19 00:01:06 +0300

    Adding Comments.
    Explaining the code in details.

--- main.py
+++ main.py
@@ -9,10 +9,14 @@ intents = discord.Intents(
     messages=True
 )
 
-bot = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix="prefix here", intents=intents)
 
 
 def checkIfUserIsStreaming(username):
+    """
+    A function that checks if a Twitch user with the provided username is live. If the user is live it returns true, otherwise returns false.
+    """
+    
     url = "https://gql.twitch.tv/gql"
     query = """query($login: String) {
   user(login: $login) {
@@ -34,36 +38,46 @@ async def on_ready():
     """
 
     print('\033[32m' + "Status: active")
-
+    
+    
     @tasks.loop(seconds=10)
     async def is_streamer_live():
+        # Loading the twitch.json config file.
         with open('twitch.json', 'r') as f:
             twitch = json.load(f)
-
-        channel = bot.get_channel(976532019674742864)
+            
+        channel = bot.get_channel("your channel id here")
+        
+        # Setting the value of the 'twitchUsername' key of the twitch.json to the username variable.
         username = twitch['twitchUsername']
-        print(checkIfUserIsStreaming(username))
+        
+        # Checking if the streamer is live.
         if checkIfUserIsStreaming(username):
+            # Since we loop through the function every 10 seconds, we don't want ev every time to send a message to the channel, so we use a variable in the json
+            # to check if we already sent a message.
             if twitch['isLive'] == 0:
                 twitch['isLive'] = 1
-                print(twitch['isLive'])
+                
+                # Creating an embed message.
                 embed = discord.Embed(colour=discord.Colour.purple(),
                                       title=f'🟣 - {username} is live!')
-                embed.set_thumbnail(url='https://static-cdn.jtvnw.net/jtv_user_pictures/50dd15e2-beb9-4058-abc1-'
-                                        '77a29c6b896b-profile_image-70x70.png')
+                embed.set_thumbnail(url="url for twitch profile picture")
                 embed.add_field(name='Channel:', value=f'https://www.twitch.tv/{username}')
+                
+                # Sending the embed message to the provided channel.
                 await channel.send(f"Hey, @everyone! {username} is live! Go check out the stream!")
                 await channel.send(embed=embed)
             else:
                 pass
         else:
             twitch['isLive'] = 0
-
+        
+        # Commiting the changes we made to the twitch.json file.
         with open('twitch.json', 'w') as f:
             json.dump(twitch, f)
+    # Starting the loop of the function.
     is_streamer_live.start()
 
-
-
-bot.run('OTc2NTQwNTI4OTgyMjI5MDUy.GDW6XV.fpw1BLeIfRPpdyic-HXI_QYpbgJK_wULFFR4ss')
+# Run the bot.
+bot.run('your bot token here')
 
